6e553c0d578cd381acac7c70f4b8657725e7059a
Hariom Tirkamte <hariomtirkamte@gmail.com>
2025-10-09 09:01:37 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,25 @@
+class Demo
+{
+    public int iNo1;
+    public int iNo2;
+
+    public Demo()
+    {
+        System.out.println("Inside Default Constructor");
+    }
+
+    public Demo(int i,int j)
+    {
+        System.out.println("Inside Parametrised Constructor");
+    }
+
+}
+class Constructor
+{
+    public static void main(String A[])
+    {
+        Demo dobj1 = new Demo();
+
+        Demo dobj2 = new Demo(11,21);
+    }
+}
